008f0ba0324c5b682264bf3b7e9004b94f16b4e3
Victor Martins <victorfrontend@gmail.com>
2019-10-21 20:58:01 -0300

    feat(Plan): create plan feature

---
+++ api/src/app/controllers/PlanController.js
@@ -0,0 +1,107 @@
+import * as Yup from 'yup';
+
+import User from '../models/User';
+import Plan from '../models/Plan';
+
+class PlanController {
+  async list(req, res) {
+    const checkIsAdmin = await User.findOne({
+      where: { id: req.userId, admin: true }
+    });
+
+    if (!checkIsAdmin) {
+      return res
+        .status(401)
+        .json({ error: 'Only users admins can list plans.' });
+    }
+
+    const plans = await Plan.findAll({
+      attributes: ['id', 'title', 'duration', 'price'],
+      order: ['id']
+    });
+
+    return res.json(plans);
+  }
+
+  async create(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      duration: Yup.number().required(),
+      price: Yup.number().required()
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed.' });
+    }
+
+    const checkIsAdmin = await User.findOne({
+      where: { id: req.userId, admin: true }
+    });
+
+    if (!checkIsAdmin) {
+      return res
+        .status(401)
+        .json({ error: 'Only users admins can create plans.' });
+    }
+
+    const { id, title, duration, price } = await Plan.create(req.body);
+
+    return res.json({ id, title, duration, price });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string(),
+      duration: Yup.number(),
+      price: Yup.number()
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed.' });
+    }
+
+    const checkIsAdmin = await User.findOne({
+      where: { id: req.userId, admin: true }
+    });
+
+    if (!checkIsAdmin) {
+      return res
+        .status(401)
+        .json({ error: 'Only users admins can update plans.' });
+    }
+
+    const plan = await Plan.findByPk(req.params.plan_id);
+
+    if (!plan) {
+      return res.status(400).json({ error: 'Plan does not exists.' });
+    }
+
+    const { id, title, duration, price } = await plan.update(req.body);
+
+    return res.json({ id, title, duration, price });
+  }
+
+  async delete(req, res) {
+    const checkIsAdmin = await User.findOne({
+      where: { id: req.userId, admin: true }
+    });
+
+    if (!checkIsAdmin) {
+      return res
+        .status(401)
+        .json({ error: 'Only users admins can delete plans.' });
+    }
+
+    const plan = await Plan.findByPk(req.params.plan_id);
+
+    if (!plan) {
+      return res.status(400).json({ error: 'Plan does not exists.' });
+    }
+
+    await plan.destroy();
+
+    return res.json({ message: `Plan was successful removed.` });
+  }
+}
+
+export default new PlanController();
--- api/src/routes.js
+++ api/src/routes.js
@@ -5,6 +5,7 @@ import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
+import PlanController from './app/controllers/PlanController';
 
 const routes = new Router();
 
@@ -17,5 +18,10 @@ routes.put('/users', UserController.update);
 
 routes.post('/students', StudentController.create);
 routes.put('/students/:student_id', StudentController.update);
+
+routes.get('/plans', PlanController.list);
+routes.post('/plans', PlanController.create);
+routes.put('/plans/:plan_id', PlanController.update);
+routes.delete('/plans/:plan_id', PlanController.delete);
 
 export default routes;
