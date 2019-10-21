467c4c036452be55b46dcb84c31308f5fac7fce1
Victor Martins <victorfrontend@gmail.com>
2019-10-21 20:59:25 -0300

    fix(User): change provider column to admin

--- api/src/app/controllers/UserController.js
+++ api/src/app/controllers/UserController.js
@@ -24,9 +24,9 @@ class UserController {
       return res.status(400).json({ error: 'User already exists.' });
     }
 
-    const { id, name, email, provider } = await User.create(req.body);
+    const { id, name, email, admin } = await User.create(req.body);
 
-    return res.json({ id, name, email, provider });
+    return res.json({ id, name, email, admin });
   }
 
   async update(req, res) {
@@ -66,9 +66,9 @@ class UserController {
       return res.status(401).json({ error: 'Password does not match.' });
     }
 
-    const { id, name, provider } = await user.update(req.body);
+    const { id, name, admin } = await user.update(req.body);
 
-    return res.json({ id, name, email, provider });
+    return res.json({ id, name, email, admin });
   }
 }
 
